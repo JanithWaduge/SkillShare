474f8281986a78def8bac6f8f51fb1482f8ed2a6
RashmiFernando <it22177414@my.sliit.lk>
2025-05-07 06:48:25 +0530

    Resolved merge conflicts

--- src/main/java/com/PAF/SkillShare/controller/TutorialController.java
+++ src/main/java/com/PAF/SkillShare/controller/TutorialController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RequestMapping("/api/tutorials")
 @CrossOrigin(origins = "*")
 public class TutorialController {
-
+///check error
     @Autowired
     private TutorialService tutorialService;
 
