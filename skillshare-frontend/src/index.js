87a6a8cf8e8e9f6bb70868f81aaacfff5005b0e2
DMDMHansika <mayashihans48@gmail.com>
2025-05-17 03:35:39 +0530

    User and Profile Management create

--- skillshare-frontend/src/index.js
+++ skillshare-frontend/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import SplashScreen from './SplashScreen';
+import SplashScreen from './pages/SplashScreen';
+import AuthScreen from './pages/AuthScreen';
 import App from './App';
 import Tutorials from './TutorialList';
 import LearnPage from './LearnPage';
 import ViewAllTutorials from './ViewAllTutorialsAdmin';
+import Profile from './components/Profile';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -15,7 +17,9 @@ root.render(
     <Router>
       <Routes>
         <Route path="/" element={<SplashScreen />} />
+        <Route path="/auth" element={<AuthScreen />} />
         <Route path="/posts" element={<App />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="/tutorials" element={<Tutorials />} />
         <Route path="/learn/:id" element={<LearnPage />} />
         <Route path="/admin/tutorials" element={<ViewAllTutorials />} />
