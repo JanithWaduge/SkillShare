be643431ab077de997878009fb59c6f979f71d05
DMDMHansika <mayashihans48@gmail.com>
2025-05-17 19:28:26 +0530

    Add profile path

--- skillshare-frontend/src/index.js
+++ skillshare-frontend/src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import SplashScreen from './SplashScreen';
+import SplashScreen from './pages/SplashScreen';
+import AuthScreen from './pages/AuthScreen';
 import App from './App';
 import Tutorials from './TutorialList';
 import LearnPage from './LearnPage';
 import ViewAllTutorials from './ViewAllTutorialsAdmin';
 import AddTutorialForm from './AddTutorialForm';
 import TutorialForm from './TutorialForm';
+import Profile from './components/Profile';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -17,7 +19,9 @@ root.render(
     <Router>
       <Routes>
         <Route path="/" element={<SplashScreen />} />
+        <Route path="/auth" element={<AuthScreen />} />
         <Route path="/posts" element={<App />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="/tutorials" element={<Tutorials />} />
         <Route path="/learn/:id" element={<LearnPage />} />
         <Route path="/admin/tutorials" element={<ViewAllTutorials />} />
